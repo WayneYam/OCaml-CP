afb6f58e73f434f6c875565e6ff78e2ea1c8ff12
WayneYam <dd490mp@gmail.com>
2025-02-05 21:25:28 +0800

    passed https://dmoj.ca/problem/aac1p4 with a better complexity

--- bin/aac1/p4.ml
+++ bin/aac1/p4.ml
@@ -24,37 +24,48 @@ let get_factor_list range =
 ;;
 
 let () =
-  let factor_list = get_factor_list 100000 in
+  let range = 100000 in
+  let factor_list = get_factor_list range in
   (* print_s @@ (sexp_of_array @@ sexp_of_list @@ sexp_of_int) factor_list; *)
-  match read_int_list () with
-  | [ n; q ] ->
-    let array = read_int_list () in
-    let positions = Array.init 100001 ~f:(fun id -> Int_set.empty) in
-    List.iteri ~f:(fun id v -> positions.(v) <- Set.add positions.(v) (id + 1)) array;
-    let answer_query l r x =
-      let test_value t =
-        let search () =
-          match
-            Set.binary_search
-              positions.(t)
-              `First_greater_than_or_equal_to
-              ~compare:[%compare: int]
-              l
-          with
-          | None -> false
-          | Some id -> if id <= r then true else false
-        in
-        search ()
+  let n, q = to_2ple @@ read_int_list () in
+  let array = read_int_list () in
+  let queries =
+    List.sort ~compare:[%compare: int * int * int * int]
+    @@ List.init q ~f:(fun id ->
+      let l, r, x = to_3ple @@ read_int_list () in
+      l, r, x, q - 1 - id)
+  in
+  let next_occurence = Array.init (range + 1) ~f:(fun x -> None) in
+  let answer = Array.init q ~f:(fun x -> false) in
+  let rec calc_answer lst queries idx =
+    match lst with
+    | [] -> queries
+    | hd :: tl ->
+      let rest_queries = calc_answer tl queries (idx + 1) in
+      next_occurence.(hd) <- Some idx;
+      let test x r =
+        match next_occurence.(x) with
+        | None -> false
+        | Some r' -> r' <= r
       in
-      let test_factor t = test_value t && test_value (x / t) in
-      List.exists ~f:test_factor factor_list.(x)
-    in
-    for i = 1 to q do
-      match read_int_list () with
-      | [ l; r; x ] -> print_string (if answer_query l r x then "YES\n" else "NO\n")
-      | _ -> assert false
-    done
-  | _ -> assert false
+      let solve_l x r qid =
+        let factors = factor_list.(x) in
+        answer.(qid) <- List.exists ~f:(fun t -> test t r && test (x / t) r) factors
+      in
+      let rec calc_l queries =
+        match queries with
+        | [] -> []
+        | (l, r, x, qid) :: tl ->
+          if l = idx
+          then (
+            solve_l x r qid;
+            calc_l tl)
+          else queries
+      in
+      calc_l rest_queries
+  in
+  calc_answer array (List.rev queries) 1 |> ignore;
+  Array.iter ~f:(fun x -> print_string @@ if x then "YES\n" else "NO\n") answer
 ;;
 
 (** End of file *)
--- lib/input.ml
+++ lib/input.ml
@@ -14,3 +14,21 @@ let read_int64 () = Int64.of_string @@ read_line ()
 let read_int64_list ?(on = [ ' ' ]) () =
   List.map ~f:Int64.of_string @@ String.split_on_chars ~on @@ read_line ()
 ;;
+
+let to_2ple l =
+  match l with
+  | [ x; y ] -> x, y
+  | _ -> assert false
+;;
+
+let to_3ple l =
+  match l with
+  | [ x; y; z ] -> x, y, z
+  | _ -> assert false
+;;
+
+let to_4ple l =
+  match l with
+  | [ x; y; z; w ] -> x, y, z, w
+  | _ -> assert false
+;;
