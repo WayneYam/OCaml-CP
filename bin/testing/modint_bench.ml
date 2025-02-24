3377e97be39957318769eddab1ac278126fa5904
WayneYam <dd490mp@gmail.com>
2025-02-24 22:40:57 +0800

    modified name

--- bin/testing/modint_bench.ml
+++ bin/testing/modint_bench.ml
@@ -7,8 +7,8 @@ open! Lib
 
 open! Input
 
-let test_int64 () =
-  let m = 998244353L in
+let test_int64 m =
+  let m = Int64.of_int m in
   let sz = 100000000 in
   let rec calc acc i =
     if i > sz then acc else calc Int64.(acc * Int64.of_int i % m) (i + 1)
@@ -16,16 +16,14 @@ let test_int64 () =
   printf "%d\n" @@ Int.of_int64_exn (calc 1L 1)
 ;;
 
-let test_int () =
-  let m = 998244353 in
+let test_int m =
   let sz = 100000000 in
   let rec calc acc i = if i > sz then acc else calc (acc * i mod m) (i + 1) in
   printf "%d\n" @@ calc 1 1
 ;;
 
-module M = (val Modint.make 998244353 : Modint.Modint)
-
-let test_modint () =
+let test_modint m =
+  let module M = (val Modint.make m : Modint.Modint) in
   let sz = 100000000 in
   let rec calc acc i =
     let i' = M.of_int i in
@@ -34,6 +32,9 @@ let test_modint () =
   printf "%d\n" @@ M.to_int @@ calc (M.of_int 1) 1
 ;;
 
-let () = test_modint ()
+let () =
+  let m = read_int () in
+  test_modint m
+;;
 
 (** End of file *)
